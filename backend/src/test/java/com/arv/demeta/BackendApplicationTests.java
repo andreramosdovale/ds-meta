bbef5f3284b99e0b181e367db0d03e2b6b931130
André Ramos <andre_ramosdovale@outlook.com>
2022-07-11 09:30:26 -0300

    setup backend

---
+++ backend/src/test/java/com/arv/demeta/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arv.demeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
