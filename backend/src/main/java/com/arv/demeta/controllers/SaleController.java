8e189551dc0a0dc2ff776ed7c1deb3ad46c66a2c
André Ramos <andre_ramosdovale@outlook.com>
2022-07-16 11:29:21 -0300

    implement search sale from date

--- backend/src/main/java/com/arv/demeta/controllers/SaleController.java
+++ backend/src/main/java/com/arv/demeta/controllers/SaleController.java
@@ -3,8 +3,11 @@ package com.arv.demeta.controllers;
 import com.arv.demeta.entities.Sale;
 import com.arv.demeta.service.SaleService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -16,7 +19,11 @@ public class SaleController {
     SaleService service;
 
     @GetMapping
-    public List<Sale> findSale() {
-        return service.findSales();
+    public Page<Sale> findSale(
+            @RequestParam(value="minDate", defaultValue = "") String minDate,
+            @RequestParam(value="maxDate", defaultValue = "") String maxDate,
+            Pageable pegeable
+    ) {
+        return service.findSales(minDate, maxDate, pegeable);
     }
 }
